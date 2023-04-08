5f9f11c4c33b1aca87f6d9f193cea9b96eb40d64
Ajay <ajaysatpati9@gmail.com>
2023-04-08 19:26:52 +0530

    Fifth Commit

--- app/src/main/java/com/example/cashmates/Analyze.kt
+++ app/src/main/java/com/example/cashmates/Analyze.kt
@@ -1,12 +1,43 @@
 package com.example.cashmates
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Analyze : AppCompatActivity() {
+    private val db = Firebase.firestore
+    private val myCollectionRef = db.collection("posts")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analyze)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        readData(object: MyCallback {
+            override fun onCallback(value: HashMap<String,Int>) {
+                Log.d("TAG", value.size.toString())
+            }
+        })
+    }
+    private fun readData(myCallback : MyCallback) {
+        myCollectionRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val hashMap : HashMap<String, Int> = HashMap()
+                for (document in task.result) {
+                    val amount = document.getString("amount")?.let { Integer.parseInt(it) }
+                    val locate = document.getString("locate").toString()
+                    if (hashMap.containsKey(locate)) {
+                        val count = hashMap[locate] ?: 0
+                        hashMap[locate] = count + amount!!
+                    } else {
+                        hashMap[locate] = amount!!
+                    }
+                }
+                myCallback.onCallback(hashMap)
+            }
+        }
+    }
+    interface MyCallback {
+        fun onCallback(value: HashMap<String,Int>)
     }
 }
--- app/src/main/java/com/example/cashmates/MainActivity.kt
+++ app/src/main/java/com/example/cashmates/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.cashmates
 
-import android.content.DialogInterface
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cashmates.daos.PostDao
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
