bd9904b725e61ebd5e7434927b06be08e58f6972
Ajay <ajaysatpati9@gmail.com>
2023-04-14 03:14:28 +0530

    Eighth Commit

--- app/src/main/java/com/example/cashmates/Analyze.kt
+++ app/src/main/java/com/example/cashmates/Analyze.kt
@@ -6,14 +6,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_analyze.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -27,10 +25,23 @@ class Analyze : AppCompatActivity() {
         readData(object: MyCallback {
             override fun onCallback(value: HashMap<String,Int>, value2: MutableMap<String,Int>) {
 
+//                BarGraph
                 val sortedMap: MutableMap<String, Int> = TreeMap(value2)
                 val barChart: BarChart = findViewById(R.id.barChart)
                 val list2: ArrayList<BarEntry> = ArrayList()
+                for ((keys, values) in sortedMap.entries) {
+                    list2.add(BarEntry(keys.toFloat(),values.toFloat()))
+                }
+                val barDataSet = BarDataSet(list2,"List")
+                barDataSet.setColors(ColorTemplate.COLORFUL_COLORS,255)
+                barDataSet.valueTextColor=Color.BLACK
+                val barData = BarData(barDataSet)
+                barChart.setFitBars(true)
+                barChart.data = barData
+                barChart.description.text = "Bar Chart"
+                barChart.animateY(2000)
 
+//                PieChart
                 val pieChart: PieChart = findViewById(R.id.pieChart)
                 val list: ArrayList<PieEntry> = ArrayList()
                 value["Food"]?.let { PieEntry(it.toFloat(),"Food") }?.let { list.add(it) }
@@ -56,7 +67,7 @@ class Analyze : AppCompatActivity() {
         myCollectionRef.whereEqualTo("createdBy.uid", userId).get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val hashMap : HashMap<String, Int> = HashMap()
-                var hash: MutableMap<String, Int>  = HashMap()
+                val hash: MutableMap<String, Int>  = HashMap()
                 for (document in task.result) {
                     val time = document.getLong("createdAt")
                     val parsedTime = time?.let { convertLongToTime(it) }
@@ -83,7 +94,7 @@ class Analyze : AppCompatActivity() {
     @SuppressLint("SimpleDateFormat")
     fun convertLongToTime(time: Long): String {
         val date = Date(time)
-        val format = SimpleDateFormat("yyyy.MM.dd")
+        val format = SimpleDateFormat("yyyy.MM")
         return format.format(date)
     }
     interface MyCallback {
