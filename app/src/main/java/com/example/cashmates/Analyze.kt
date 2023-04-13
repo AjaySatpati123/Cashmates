8f6a11f0cbb8c852e06803233a78a9fd86d3acdf
Ajay <ajaysatpati9@gmail.com>
2023-04-13 23:46:21 +0530

    Eighth Commit

--- app/src/main/java/com/example/cashmates/Analyze.kt
+++ app/src/main/java/com/example/cashmates/Analyze.kt
@@ -1,9 +1,11 @@
 package com.example.cashmates
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
@@ -12,6 +14,8 @@ import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Analyze : AppCompatActivity() {
     private val db = Firebase.firestore
@@ -21,8 +25,11 @@ class Analyze : AppCompatActivity() {
         setContentView(R.layout.activity_analyze)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         readData(object: MyCallback {
-            override fun onCallback(value: HashMap<String,Int>) {
-                Log.d("TAG", value.size.toString())
+            override fun onCallback(value: HashMap<String,Int>, value2: HashMap<String,Int>) {
+                Log.d("Problem","${value2.size}")
+
+                val barGraph: BarChart
+
                 val pieChart: PieChart = findViewById(R.id.pieChart)
                 val list: ArrayList<PieEntry> = ArrayList()
                 value["Food"]?.let { PieEntry(it.toFloat(),"Food") }?.let { list.add(it) }
@@ -48,7 +55,10 @@ class Analyze : AppCompatActivity() {
         myCollectionRef.whereEqualTo("createdBy.uid", userId).get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val hashMap : HashMap<String, Int> = HashMap()
+                val hash : HashMap<String, Int> = HashMap()
                 for (document in task.result) {
+                    val time = document.getLong("createdAt")
+                    val parsedTime = time?.let { convertLongToTime(it) }
                     val amount = document.getString("amount")?.let { Integer.parseInt(it) }
                     val locate = document.getString("locate").toString()
                     if (hashMap.containsKey(locate)) {
@@ -57,12 +67,25 @@ class Analyze : AppCompatActivity() {
                     } else {
                         hashMap[locate] = amount!!
                     }
+                    if(hash.containsKey(parsedTime)){
+                        val count = hash[parsedTime] ?: 0
+                        hash[parsedTime!!]=count+amount
+                    } else {
+                        hash[parsedTime!!]=amount
+                    }
                 }
-                myCallback.onCallback(hashMap)
+
+                myCallback.onCallback(hashMap,hash)
             }
         }
     }
+    @SuppressLint("SimpleDateFormat")
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy.MM.dd")
+        return format.format(date)
+    }
     interface MyCallback {
-        fun onCallback(value: HashMap<String,Int>)
+        fun onCallback(value: HashMap<String,Int>, value2: HashMap<String,Int>)
     }
 }
