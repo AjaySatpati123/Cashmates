f38bc065c084b947d4ba173b48267c37ffd1bca7
Ajay <ajaysatpati9@gmail.com>
2023-04-26 20:40:44 +0530

    twelfth Commit

--- app/src/main/java/com/example/cashmates/CreateNoteActivity.kt
+++ app/src/main/java/com/example/cashmates/CreateNoteActivity.kt
@@ -2,10 +2,20 @@ package com.example.cashmates
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cashmates.daos.NoteDao
+import kotlinx.android.synthetic.main.activity_create_note.*
 
 class CreateNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_note)
+        noteButton.setOnClickListener {
+            val input = noteInput.text.toString().trim()
+            if(input.isNotEmpty()){
+                val noteDao = NoteDao()
+                noteDao.addNote(input)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/cashmates/Notes.kt
+++ app/src/main/java/com/example/cashmates/Notes.kt
@@ -1,5 +1,6 @@
 package com.example.cashmates
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_notes.*
@@ -10,7 +11,8 @@ class Notes : AppCompatActivity() {
         setContentView(R.layout.activity_notes)
 
         fab.setOnClickListener {
-
+            val intent3 = Intent(this, CreateNoteActivity::class.java)
+            startActivity(intent3)
         }
     }
 }
